679de94c3c292519f4bd3c2ed4f58287bc81d0d9
RPIFisherman <yuyanggong.rpi@gmail.com>
2024-07-04 14:47:10 +0800

    from stkovflw

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Introduction
 
+Extend from the [APS-MES](https://github.com/RPIFisherman/APS-MES). This project add a graphical user interface (GUI) to the APS application. The GUI is implemented using the [javafx](https://openjfx.io/) library.
+
 [//]: # (This project is an implementation of an APS application that uses the [Gurobi]&#40;https://www.gurobi.com/&#41; optimization solver to solve the scheduling problem. The application is designed to be used by a manufacturing company to schedule the production of products in a way that maximizes the company's profit while meeting customer demand and respecting production constraints)
--- src/main/java/GanttChart.java
+++ src/main/java/GanttChart.java
@@ -1,18 +1,177 @@
 package ygong.APS;
 
+import javafx.beans.NamedArg;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.chart.Axis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 
-public class GanttChart extends XYChart<Number, String> {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GanttChart<X, Y> extends XYChart<X, Y> {
+
+    private double blockHeight = 10;
+
+    public GanttChart(@NamedArg("xAxis") Axis<X> xAxis, @NamedArg("yAxis") Axis<Y> yAxis) {
+        this(xAxis, yAxis, FXCollections.observableArrayList());
+    }
+
+    public GanttChart(@NamedArg("xAxis") Axis<X> xAxis, @NamedArg("yAxis") Axis<Y> yAxis, @NamedArg("data") ObservableList<Series<X, Y>> data) {
+        super(xAxis, yAxis);
+        if (!(xAxis instanceof ValueAxis && yAxis instanceof CategoryAxis)) {
+            throw new IllegalArgumentException("Axis type incorrect, X and Y should both be NumberAxis");
+        }
+        setData(data);
+    }
+
+    private static String getStyleClass(Object obj) {
+        return ((ExtraData) obj).getStyleClass();
+    }
+
+    private static double getLength(Object obj) {
+        return ((ExtraData) obj).getLength();
+    }
+
+    @Override
+    protected void layoutPlotChildren() {
+
+        for (int seriesIndex = 0; seriesIndex < getData().size(); seriesIndex++) {
+
+            Series<X, Y> series = getData().get(seriesIndex);
+
+            Iterator<Data<X, Y>> iter = getDisplayedDataIterator(series);
+            while (iter.hasNext()) {
+                Data<X, Y> item = iter.next();
+                double x = getXAxis().getDisplayPosition(item.getXValue());
+                double y = getYAxis().getDisplayPosition(item.getYValue());
+                if (Double.isNaN(x) || Double.isNaN(y)) {
+                    continue;
+                }
+                Node block = item.getNode();
+                Rectangle ellipse;
+                if (block != null) {
+                    if (block instanceof StackPane) {
+                        StackPane region = (StackPane) item.getNode();
+                        if (region.getShape() == null) {
+                            ellipse = new Rectangle(getLength(item.getExtraValue()), getBlockHeight());
+                        } else if (region.getShape() instanceof Rectangle) {
+                            ellipse = (Rectangle) region.getShape();
+                        } else {
+                            return;
+                        }
+                        ellipse.setWidth(getLength(item.getExtraValue()) * ((getXAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis) getXAxis()).getScale()) : 1));
+                        ellipse.setHeight(getBlockHeight() * ((getYAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis) getYAxis()).getScale()) : 1));
+                        y -= getBlockHeight() / 2.0;
+
+                        // Note: workaround for RT-7689 - saw this in ProgressControlSkin
+                        // The region doesn't update itself when the shape is mutated in place, so we
+                        // null out and then restore the shape in order to force invalidation.
+                        region.setShape(null);
+                        region.setShape(ellipse);
+                        region.setScaleShape(false);
+                        region.setCenterShape(false);
+                        region.setCacheShape(false);
+
+                        block.setLayoutX(x);
+                        block.setLayoutY(y);
+                    }
+                }
+            }
+        }
+    }
+
+    public double getBlockHeight() {
+        return blockHeight;
+    }
+
+    public void setBlockHeight(double blockHeight) {
+        this.blockHeight = blockHeight;
+    }
+
+    @Override
+    protected void dataItemAdded(Series<X, Y> series, int itemIndex, Data<X, Y> item) {
+        Node block = createContainer(series, getData().indexOf(series), item, itemIndex);
+        getPlotChildren().add(block);
+    }
+
+    @Override
+    protected void dataItemRemoved(final Data<X, Y> item, final Series<X, Y> series) {
+        final Node block = item.getNode();
+        getPlotChildren().remove(block);
+        removeDataItemFromDisplay(series, item);
+    }
+
+    @Override
+    protected void dataItemChanged(Data<X, Y> item) {
+    }
+
+    @Override
+    protected void seriesAdded(Series<X, Y> series, int seriesIndex) {
+        for (int j = 0; j < series.getData().size(); j++) {
+            Data<X, Y> item = series.getData().get(j);
+            Node container = createContainer(series, seriesIndex, item, j);
+            getPlotChildren().add(container);
+        }
+    }
+
+    @Override
+    protected void seriesRemoved(final Series<X, Y> series) {
+        for (XYChart.Data<X, Y> d : series.getData()) {
+            final Node container = d.getNode();
+            getPlotChildren().remove(container);
+        }
+        removeSeriesFromDisplay(series);
+
+    }
+
+    private Node createContainer(Series<X, Y> series, int seriesIndex, final Data<X, Y> item, int itemIndex) {
+
+        Node container = item.getNode();
+
+        if (container == null) {
+            container = new StackPane();
+            item.setNode(container);
+        }
+
+        container.getStyleClass().add(getStyleClass(item.getExtraValue()));
+
+        return container;
+    }
+
+    @Override
+    protected void updateAxisRange() {
+        final Axis<X> xa = getXAxis();
+        final Axis<Y> ya = getYAxis();
+        List<X> xData = null;
+        List<Y> yData = null;
+        if (xa.isAutoRanging()) xData = new ArrayList<X>();
+        if (ya.isAutoRanging()) yData = new ArrayList<Y>();
+        if (xData != null || yData != null) {
+            for (Series<X, Y> series : getData()) {
+                for (Data<X, Y> data : series.getData()) {
+                    if (xData != null) {
+                        xData.add(data.getXValue());
+                        xData.add(xa.toRealValue(xa.toNumericValue(data.getXValue()) + getLength(data.getExtraValue())));
+                    }
+                    if (yData != null) {
+                        yData.add(data.getYValue());
+                    }
+                }
+            }
+            if (xData != null) xa.invalidateRange(xData);
+            if (yData != null) ya.invalidateRange(yData);
+        }
+    }
 
     public static class ExtraData {
+
         public long length;
         public String styleClass;
+
 
         public ExtraData(long lengthMs, String styleClass) {
             super();
@@ -24,69 +183,19 @@ public class GanttChart extends XYChart<Number, String> {
             return length;
         }
 
+        public void setLength(long length) {
+            this.length = length;
+        }
+
         public String getStyleClass() {
             return styleClass;
         }
-    }
 
-    public GanttChart(Axis<Number> xAxis, Axis<String> yAxis) {
-        super(xAxis, yAxis);
-        setData(FXCollections.observableArrayList());
-    }
-
-    @Override
-    protected void layoutPlotChildren() {
-        for (Series<Number, String> series : getData()) {
-            for (Data<Number, String> item : series.getData()) {
-                double x = getXAxis().getDisplayPosition(item.getXValue());
-                double y = getYAxis().getDisplayPosition(item.getYValue());
-                double width = getXAxis().getDisplayPosition(item.getXValue().doubleValue() + ((ExtraData) item.getExtraValue()).getLength()) - x;
-                double height = 10;
-
-                StackPane rectangle = (StackPane) item.getNode();
-                rectangle.setLayoutX(x);
-                rectangle.setLayoutY(y - height / 2.0);
-                rectangle.setPrefWidth(width);
-                rectangle.setPrefHeight(height);
-            }
+        public void setStyleClass(String styleClass) {
+            this.styleClass = styleClass;
         }
+
+
     }
 
-    @Override
-    protected void dataItemAdded(Series<Number, String> series, int itemIndex, Data<Number, String> item) {
-        Node block = createContainer(series, getData().indexOf(series), item, itemIndex);
-        getPlotChildren().add(block);
-    }
-
-    @Override
-    protected void dataItemRemoved(Data<Number, String> item, Series<Number, String> series) {
-        getPlotChildren().remove(item.getNode());
-    }
-
-    @Override
-    protected void dataItemChanged(Data<Number, String> item) {
-    }
-
-    @Override
-    protected void seriesAdded(Series<Number, String> series, int seriesIndex) {
-        for (int j = 0; j < series.getData().size(); j++) {
-            Data<Number, String> item = series.getData().get(j);
-            Node container = createContainer(series, seriesIndex, item, j);
-            getPlotChildren().add(container);
-        }
-    }
-
-    @Override
-    protected void seriesRemoved(Series<Number, String> series) {
-        for (XYChart.Data<Number, String> d : series.getData()) {
-            getPlotChildren().remove(d.getNode());
-        }
-    }
-
-    private Node createContainer(Series<Number, String> series, int seriesIndex, final Data<Number, String> item, int itemIndex) {
-        StackPane container = new StackPane();
-        container.getStyleClass().add(((ExtraData) item.getExtraValue()).getStyleClass());
-        item.setNode(container);
-        return container;
-    }
 }
