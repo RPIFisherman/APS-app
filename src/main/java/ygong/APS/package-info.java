3706eaadbc6f0a65875f04a024240f8e145b7cf4
RPIFisherman <yuyanggong.rpi@gmail.com>
2024-07-31 13:42:42 +0800

    add package-info.java, remove structure diagrams

---
+++ src/main/java/ygong/APS/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * This package contains the classes that are used to implement the APS
+ * (Advanced Planning and Scheduling) project. The APS project is a project that
+ * aims to optimize the scheduling of tasks in a production environment. The
+ * project is implemented in Java and uses JavaFX for the GUI. The project
+ * consists of several classes that implement the different components of the
+ * APS system, such as the scheduler, the schedule, and the Gantt chart.
+ * <p>
+ * This is a personal project that I implemented during my internship at <a
+ * href="https://www.linkedata.com.cn/">Linkedata</a>. The project is just a
+ * prototype and is not intended for production use.
+ * </p>
+ *
+ * @note We are only consider a order finished on a single machine which is a <a href="https://en.wikipedia.org/wiki/Unrelated-machines_scheduling"> unrelated machine scheduling problem</a> , not a <a href="https://en.wikipedia.org/wiki/Job-shop_scheduling" > job-shop schduling problem</a>
+ * @author <a href="mailto:yuyanggong.rpi@gmail.com">Yuyang Gong</a>
+ * @version 1.0
+ * @since 1.0
+ */
+package ygong.APS;
