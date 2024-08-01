0d79e1f666490e6864c1972bbaeaac9ea047545f
RPIFisherman <yuyanggong.rpi@gmail.com>
2024-08-01 15:57:22 +0800

    update javadoc using Java 21

--- docs/javadocs/package-search-index.js
+++ docs/javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ygong.APS"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ygong.APS"}];updateSearchResults();
